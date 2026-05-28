382dba01691eedfc8e4c06a3fde46755fedda4b8
sebastianp-as <sebastian.padilla@assuresoft.com>
2026-05-28 17:34:36 -0400

    REFACTOR: introduce symbol table greedy algorithm

--- src/romanNumeral.js
+++ src/romanNumeral.js
@@ -1,31 +1,22 @@
+const romaSymbols = [
+    { value: 10, symbol: 'X' },
+    { value: 9, symbol: 'IX' },
+    { value: 5, symbol: 'V' },
+    { value: 4, symbol: 'IV' },
+    { value: 1, symbol: 'I' },
+];
+
 function toRoman(number) {
-    if (number <= 3) {
-        return 'I'.repeat(number);
+    let result = '';
+
+    for (const { value, symbol } of romaSymbols) {
+        while (number >= value) {
+            result += symbol;
+            number -= value;
+        }
     }
 
-    if (number === 4) {
-        return 'IV';
-    }
-
-    if (number === 5) {
-        return 'V';
-    }
-
-    if (number > 5 && number < 9 ) {
-        return 'V' + 'I'.repeat(number - 5);
-    }
-
-    if (number === 9) {
-        return 'IX';
-    }
-
-    if (number === 10) {
-        return 'X';
-    }
-
-    if (number > 10 && number < 20) {
-        return 'X' + toRoman(number - 10);
-    }
+    return result;
 }
 
 module.exports = {
